f5d43c62dfa932ebb1852c67516ff8e385806d54
Chris Malloy <cjmalloy@gmail.com>
2022-04-05 17:32:03 -0300

    Edit refs

--- src/app/component/ref/ref.component.ts
+++ src/app/component/ref/ref.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, HostBinding, Input, OnInit, ViewChild } from "@angular/core";
 import { Ref } from "../../model/ref";
 import { RefService } from "../../service/ref.service";
-import { authors, interestingTags, refUrlSummary, webLink } from "../../util/format";
+import { authors, interestingTags, refUrlSummary, TAG_REGEX, webLink } from "../../util/format";
 import * as _ from "lodash";
 import { mergeMap, Observable } from "rxjs";
 import { AccountService } from "../../service/account.service";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-ref',
@@ -27,17 +28,27 @@ export class RefComponent implements OnInit {
   @ViewChild('inlineTag')
   inlineTag?: ElementRef;
 
+  editForm: FormGroup;
+  submitted = false;
   commentCount?: number;
   responseCount?: number;
   sourceCount?: number;
   expandPlugin?: string;
   tagging = false;
+  editing = false;
   writeAccess$?: Observable<boolean>;
 
   constructor(
+    private fb: FormBuilder,
     private account: AccountService,
     private refs: RefService,
-  ) { }
+  ) {
+    this.editForm = fb.group({
+      comment: [''],
+      sources: fb.array([]),
+      tags: fb.array([]),
+    });
+  }
 
   ngOnInit(): void {
     this.writeAccess$ = this.account.writeAccess(this.ref);
@@ -47,6 +58,9 @@ export class RefComponent implements OnInit {
     if (this.ref.tags) {
       this.expandPlugin = _.intersection(this.ref.tags, this.expandable)[0];
     }
+    while (this.sourcesForm.length < (this.ref?.sources?.length || 0)) this.addSource();
+    while (this.tagsForm.length < (this.ref?.tags?.length || 0)) this.addTag();
+    this.editForm.patchValue(this.ref);
   }
 
   get authors() {
@@ -63,6 +77,14 @@ export class RefComponent implements OnInit {
 
   get webLink() {
     return webLink(this.ref);
+  }
+
+  get tagsForm() {
+    return this.editForm.get('tags') as FormArray;
+  }
+
+  get sourcesForm() {
+    return this.editForm.get('sources') as FormArray;
   }
 
   get comments() {
@@ -97,6 +119,39 @@ export class RefComponent implements OnInit {
       mergeMap(() => this.refs.get(this.ref.url, this.ref.origin!)),
     ).subscribe(ref => {
       this.tagging = false;
+      this.ref = ref;
+    });
+  }
+
+  addTag(value = '') {
+    this.tagsForm.push(this.fb.control(value, [Validators.required, Validators.pattern(TAG_REGEX)]));
+    this.submitted = false;
+  }
+
+  removeTag(index: number) {
+    this.tagsForm.removeAt(index);
+  }
+
+  addSource() {
+    this.sourcesForm.push(this.fb.control('', [Validators.required]));
+    this.submitted = false;
+  }
+
+  removeSource(index: number) {
+    this.sourcesForm.removeAt(index);
+  }
+
+  save() {
+    this.submitted = true;
+    this.editForm.markAllAsTouched();
+    if (!this.editForm.valid) return;
+    this.refs.update({
+      ...this.ref,
+      ...this.editForm.value,
+    }).pipe(
+      mergeMap(() => this.refs.get(this.ref.url, this.ref.origin)),
+    ).subscribe(ref => {
+      this.editing = false;
       this.ref = ref;
     });
   }
