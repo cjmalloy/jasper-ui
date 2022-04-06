90141e969bcaa7ffb44537bf7fcd9209b84d8447
Chris Malloy <cjmalloy@gmail.com>
2022-04-06 16:46:03 -0300

    Optimized home page

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,72 +1,85 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Ref } from "../../model/ref";
 import { RefService } from "../../service/ref.service";
 import { Page } from "../../model/page";
-import { mergeMap, tap } from "rxjs/operators";
+import { distinctUntilChanged, mergeMap } from "rxjs/operators";
 import { ActivatedRoute } from "@angular/router";
 import { AccountService } from "../../service/account.service";
-import { Observable, of } from "rxjs";
+import { combineLatest, map, Observable, of, Subject, takeUntil } from "rxjs";
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
 
   page?: Page<Ref>;
-  path = 'all';
-  filter = 'new';
-  pageNumber?: number;
-  pageSize = 20;
+  defaultPageSize = 20;
 
   constructor(
     private route: ActivatedRoute,
     private account: AccountService,
     private refs: RefService,
-  ) {
-    route.url.pipe(
-      tap(segments => this.path = account.signedIn() ? segments[0].path : 'all'),
-      mergeMap(() => route.params),
-      tap(params => this.filter = params['filter']),
-      mergeMap(() => route.queryParams),
-      tap(queryParams => {
-        this.pageNumber = queryParams['pageNumber'] ?? this.pageNumber;
-        this.pageSize = queryParams['pageSize'] ?? this.pageSize;
-      }),
-    ).subscribe(() => this.refresh())
-  }
+  ) { }
 
   ngOnInit(): void {
+    combineLatest(this.path$, this.filter$, this.pageNumber$, this.pageSize$,).pipe(
+      takeUntil(this.destroy$),
+      distinctUntilChanged(_.isEqual),
+    ).subscribe(([path, filter, pageNumber, pageSize]) => this.refresh(path, filter, pageNumber, pageSize));
   }
 
-  get query(): Observable<Record<string, any>> {
-    if (this.path === 'home') {
-      if (this.filter === 'new') {
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  get path$() {
+    return this.route.url.pipe(map(segments => this.account.signedIn() ? segments[0].path : 'all'));
+  }
+
+  get filter$() {
+    return this.route.params.pipe(map(params => params['filter']));
+  }
+
+  get pageNumber$() {
+    return this.route.queryParams.pipe(map(queryParams => queryParams['pageNumber']));
+  }
+
+  get pageSize$() {
+    return this.route.queryParams.pipe(map(queryParams => queryParams['pageSize']));
+  }
+
+  getQuery(path: string, filter: string): Observable<Record<string, any>> {
+    if (path === 'home') {
+      if (filter === 'new') {
         return this.account.getMyUserExt().pipe(
           mergeMap(ext => of({ query: ext.config.subscriptions.join('+') }))
         );
       }
-      if (this.filter === 'uncited') {
+      if (filter === 'uncited') {
         return of({ uncited: true })
       }
-      if (this.filter === 'unsourced') {
+      if (filter === 'unsourced') {
         return of({ unsourced: true })
       }
-      throw `Invalid filter ${this.filter}`;
+      throw `Invalid filter ${filter}`;
     }
-    if (this.path === 'all') {
+    if (path === 'all') {
       return of({ query: '!plugin/comment@*' });
     }
-    throw `Invalid path ${this.path}`;
+    throw `Invalid path ${path}`;
   }
 
-  refresh() {
-    this.query.pipe(
+  refresh(path: string, filter: string, pageNumber?: number, pageSize?: number) {
+    this.getQuery(path, filter).pipe(
       mergeMap(query => this.refs.page({
         ...query,
-        page: this.pageNumber,
-        size: this.pageSize,
+        page: pageNumber,
+        size: pageSize ?? this.defaultPageSize,
       }))
     ).subscribe(page => this.page = page);
   }
