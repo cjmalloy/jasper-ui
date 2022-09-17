a7066061945511f0cb69a5194abf6056d7d5c25e
Chris Malloy <cjmalloy@gmail.com>
2022-09-17 17:54:31 -0300

    Origin already contains @ symbol

--- src/app/util/format.ts
+++ src/app/util/format.ts
@@ -29,7 +29,7 @@ export function hasTemplate(tags: string[], template: string) {
 }
 
 export function authors(ref: Ref) {
-  return templates(ref.tags || [], 'user').map(t => t + (ref.origin ? '@' + ref.origin : ''));
+  return templates(ref.tags || [], 'user').map(t => t + (ref.origin ? ref.origin : ''));
 }
 
 export function webLink(ref: Ref) {
