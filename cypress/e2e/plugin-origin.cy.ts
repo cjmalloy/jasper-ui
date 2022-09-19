8a60f0ef787d5f80e524e64db22e49ac2f96bd8e
Chris Malloy <cjmalloy@gmail.com>
2022-09-19 00:15:00 -0300

    Set defaults for cy:open

--- cypress/e2e/plugin-origin.cy.ts
+++ cypress/e2e/plugin-origin.cy.ts
@@ -1,6 +1,6 @@
 describe('Origin Plugin: Remote Replication', () => {
   const replUrl = Cypress.env('CYPRESS_replUrl') || 'http://localhost:8082';
-  const replApi = Cypress.env('CYPRESS_replApi') || 'http://localhost:8083';
+  const replApi = Cypress.env('CYPRESS_replApi') || 'http://repl-web';
   it('loads the page', () => {
     cy.visit('/?debug=ADMIN');
     cy.contains('Home', { timeout: 30000 });
