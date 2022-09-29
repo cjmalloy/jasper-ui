56dc697295547a1192d88c4181e0f1f83dd0e7f3
Chris Malloy <cjmalloy@gmail.com>
2022-09-29 19:26:57 -0300

    Use mochawesome for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   viewportWidth: 1025, // Prevent mobile layout
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    'baseUrl': 'http://localhost:4200',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
-  reporterOptions: {
-    mochaFile: 'cypress/results/results-[hash].xml',
-    jenkinsMode: true,
-  }
 })
