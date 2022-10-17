5e8c8ff5dc6c7e1dae54cc754cfa0238b1ee13e2
Chris Malloy <cjmalloy@gmail.com>
2022-10-17 00:44:58 -0300

    Refactored download file

--- src/app/util/download.ts
+++ src/app/util/download.ts
@@ -3,15 +3,17 @@ import * as JSZip from 'jszip';
 import { Page } from '../model/page';
 import { Tag } from '../model/tag';
 
+export function file(obj: any) {
+  return new Blob([JSON.stringify(obj, null, 2)], {type: 'text/plain;charset=utf-8'});
+}
+
 export function download(tag: Tag) {
-  const blob = new Blob([JSON.stringify(tag, null, 2)], {type: 'text/plain;charset=utf-8'});
-  FileSaver.saveAs(blob, (tag.name || tag.tag.replace('/', '_')) + '.json');
+  FileSaver.saveAs(file(tag), (tag.name || tag.tag.replace('/', '_')) + '.json');
 }
 
 export function downloadPage(type: 'ref' | 'ext' | 'user' | 'plugin' | 'template', page: Page<any>, query: String) {
-  const blob = new Blob([JSON.stringify(page.content, null, 2)], {type: 'text/plain;charset=utf-8'});
   const zip = new JSZip();
-  zip.file(type + '.json', blob);
+  zip.file(type + '.json', file(page.content));
   zip.generateAsync({ type: 'blob' })
     .then(content => FileSaver.saveAs(content, `${query.replace('/', '_')} (page ${page.number + 1} of ${page.totalPages}).zip`));
 }
