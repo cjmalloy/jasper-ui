16012fff734811e2ad25cfc618621e30d770657c
Chris Malloy <cjmalloy@gmail.com>
2022-10-23 19:51:37 -0300

    Show loading icon for restoring and uploading

--- src/app/page/settings/backup/backup.component.ts
+++ src/app/page/settings/backup/backup.component.ts
@@ -1,7 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import * as _ from 'lodash-es';
+import { catchError, forkJoin, throwError } from 'rxjs';
 import { Ext } from '../../../model/ext';
 import { Ref } from '../../../model/ref';
 import { BackupService } from '../../../service/api/backup.service';
@@ -20,6 +21,8 @@ import { getModels, getTextFile, getZipFile } from '../../../util/zip';
 export class SettingsBackupPage implements OnInit {
 
   list?: string[];
+  uploading = false;
+  restoring = false;
   serverError: string[] = [];
 
   constructor(
@@ -53,39 +56,46 @@ export class SettingsBackupPage implements OnInit {
   upload(files?: FileList) {
     this.serverError = [];
     if (!files || !files.length) return;
+    this.uploading = true;
     const file = files[0]!;
     this.backups.upload(file).pipe(
       catchError((res: HttpErrorResponse) => {
         this.serverError = printError(res);
+        this.uploading = false;
         return throwError(() => res);
       }),
-    ).subscribe(() => this.list?.unshift(files[0].name));
+    ).subscribe(() => {
+      this.uploading = false;
+      this.list?.unshift(files[0].name);
+    });
   }
 
   restore(files?: FileList) {
     this.serverError = [];
     if (!files || !files.length) return;
+    this.restoring = true;
+    const restore = (p: Promise<any>) => p
+      .catch(err => this.serverError.push(err))
+      .then(() => this.restoring = false);
     const file = files[0]!;
-    if (file.name.toLowerCase().endsWith('.zip')) {
-      getZipFile(file, 'ref.json')
-        .then(json => getModels<Ref>(json))
-        .then(refs => refs.map(r => this.uploadRef(r)))
-        .catch(err => this.serverError.push(err));
-      getZipFile(file, 'ext.json')
-        .then(json => getModels<Ext>(json))
-        .then(exts => exts.map(e => this.uploadExt(e)))
-        .catch(err => this.serverError.push(err));
-    } else if (file.name.startsWith('ref')) {
-      getTextFile(file)
-        .then(json => getModels<Ref>(json))
-        .then(refs => refs.map(r => this.uploadRef(r)))
-        .catch(err => this.serverError.push(err));
-    } else if (file.name.startsWith('ext')) {
-      getTextFile(file)
-        .then(json => getModels<Ext>(json))
-        .then(exts => exts.map(e => this.uploadExt(e)))
-        .catch(err => this.serverError.push(err));
-    }
+    _.defer(() => {
+      if (file.name.toLowerCase().endsWith('.zip')) {
+        restore(getZipFile(file, 'ref.json')
+          .then(json => getModels<Ref>(json))
+          .then(refs => refs.map(r => this.uploadRef(r)))
+          .then(() => getZipFile(file, 'ext.json'))
+          .then(json => getModels<Ext>(json))
+          .then(exts => exts.map(e => this.uploadExt(e))));
+      } else if (file.name.startsWith('ref')) {
+        restore(getTextFile(file)
+          .then(json => getModels<Ref>(json))
+          .then(refs => refs.map(r => this.uploadRef(r))));
+      } else if (file.name.startsWith('ext')) {
+        restore(getTextFile(file)
+          .then(json => getModels<Ext>(json))
+          .then(exts => exts.map(e => this.uploadExt(e))));
+      }
+    });
   }
 
   uploadRef(ref: Ref) {
