b314ba932b2c41722fc1d01dc2bfe3e99b4d7ae8
Chris Malloy <cjmalloy@gmail.com>
2022-12-24 08:04:14 -0400

    Focus after route change

--- src/app/directive/autofocus.directive.ts
+++ src/app/directive/autofocus.directive.ts
@@ -1,13 +1,26 @@
 import { Directive, ElementRef } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Directive({
   selector: '[appAutofocus]',
 })
 export class AutofocusDirective {
 
-  constructor(private elementRef: ElementRef) { };
+  constructor(
+    private elementRef: ElementRef,
+    private router: Router,
+  ) {
+    router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe(() => this.focus());
+  };
 
   ngOnInit(): void {
+    this.focus();
+  }
+
+  focus() {
     this.elementRef.nativeElement.focus();
   }
 
