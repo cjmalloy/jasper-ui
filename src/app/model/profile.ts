ee34da0fe04e7fb9a70e80f4297716e952c5a094
Chris Malloy <cjmalloy@gmail.com>
2023-02-07 11:08:41 -0400

    Renamed anon role

--- src/app/model/profile.ts
+++ src/app/model/profile.ts
@@ -10,10 +10,10 @@ export type ProfilePageArgs = {
   size?: number,
 };
 
-const roleOrder = ['ROLE_SYSADMIN', 'ROLE_ADMIN', 'ROLE_MOD', 'ROLE_EDITOR', 'ROLE_USER', 'ROLE_VIEWER', 'ROLE_ANON'];
+const roleOrder = ['ROLE_SYSADMIN', 'ROLE_ADMIN', 'ROLE_MOD', 'ROLE_EDITOR', 'ROLE_USER', 'ROLE_VIEWER', 'ROLE_ANONYMOUS'];
 
 export function getRole(...roles: (string | undefined)[]) {
-  let index = roleOrder.indexOf('ROLE_ANON');
+  let index = roleOrder.indexOf('ROLE_ANONYMOUS');
   for (const r of roles) {
     index = Math.min(index, roleOrder.indexOf(r || ''));
   }
