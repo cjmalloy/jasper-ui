bf19d0a63aa40a34bd6f5116e110405777afa16c
Chris Malloy <cjmalloy@gmail.com>
2023-04-02 17:50:29 -0300

    Fixed tests

--- src/app/service/api/origin.service.spec.ts
+++ src/app/service/api/origin.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OriginService } from './origin.service';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('OriginService', () => {
   let service: OriginService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+    });
     service = TestBed.inject(OriginService);
   });
 
