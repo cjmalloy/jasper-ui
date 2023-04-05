62871bf45b58e146fee2cdeccf674d65ba268ff1
Chris Malloy <cjmalloy@gmail.com>
2023-04-05 15:26:12 -0300

    Fixed tests

--- src/app/page/settings/origins/origins.component.spec.ts
+++ src/app/page/settings/origins/origins.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SettingsOriginsPage } from './origins.component';
 
@@ -8,7 +10,11 @@ describe('OriginsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SettingsOriginsPage ]
+      declarations: [ SettingsOriginsPage ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
     })
     .compileComponents();
 
