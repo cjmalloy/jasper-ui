4fd4ba321e4f5604d794d98e52a8a3b777c0eced
Chris Malloy <cjmalloy@gmail.com>
2023-04-10 07:23:43 -0300

    Save ref origin in toggled key

--- src/app/store/local.ts
+++ src/app/store/local.ts
@@ -1,14 +1,14 @@
 
 export class LocalStore {
 
-  isRefToggled(url: string, defaultValue = false) {
-    const value = localStorage.getItem('toggled:' + url);
+  isRefToggled(url: string, origin: string = '', defaultValue = false) {
+    const value = localStorage.getItem(`toggled:${origin}:${url}`);
     if (!value) return defaultValue;
     return value === 'true';
   }
 
-  setRefToggled(url: string, value= true) {
-    localStorage.setItem('toggled:' + url, ''+value);
+  setRefToggled(url: string, origin = '', value= true) {
+    localStorage.setItem(`toggled:${origin}:${url}`, ''+value);
   }
 
   saveEditing(text: string) {
