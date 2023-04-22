647e057202b620c8e7c038c97d72cfbc86c29c6f
Chris Malloy <cjmalloy@gmail.com>
2023-04-22 11:40:16 -0300

    Fixed tests

--- src/app/component/login-popup/login-popup.component.spec.ts
+++ src/app/component/login-popup/login-popup.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -12,6 +13,7 @@ describe('LoginPopupComponent', () => {
       declarations: [ LoginPopupComponent ],
       imports: [
         RouterTestingModule,
+        HttpClientTestingModule,
       ],
     })
     .compileComponents();
