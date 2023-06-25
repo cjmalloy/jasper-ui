e7360687cfa057ed846fa97e404a5ea0a8f8c6df
Chris Malloy <cjmalloy@gmail.com>
2023-06-25 03:18:49 -0300

    Added buttons to add new tags

---
+++ src/app/directive/input-done.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InputDoneDirective } from './input-done.directive';
+
+describe('InputDoneDirective', () => {
+  it('should create an instance', () => {
+    const directive = new InputDoneDirective();
+    expect(directive).toBeTruthy();
+  });
+});
