e4e17cf4f3b54f8e20cedc37a0477c2613313999
Chris Malloy <cjmalloy@gmail.com>
2023-07-26 16:58:50 -0300

    Fixed error messages

--- src/app/formly/errors.ts
+++ src/app/formly/errors.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { FieldType, FieldTypeConfig, FormlyField, FormlyFieldProps } from '@ngx-formly/core';
+import { Component, Input } from '@angular/core';
 import { FormlyFieldConfig } from '@ngx-formly/core/lib/models';
 
 @Component({
@@ -11,65 +10,72 @@ import { FormlyFieldConfig } from '@ngx-formly/core/lib/models';
     </span>
     <ng-template #tag>
       <span *ngIf="field.type === 'tag' && field.formControl?.errors?.['pattern'] else qtag" i18n>
-        Tags must be lower case letters, numbers, periods and forward slashes.
-        Must not start with a forward slash or period.
-        Must not or contain two forward slashes or periods in a row.
-        Protected tags start with a plus sign.
+        Tags must be lower case letters, numbers, periods and forward slashes.<br>
+        Must not start with a forward slash or period.<br>
+        Must not or contain two forward slashes or periods in a row.<br>
+        Protected tags start with a plus sign.<br>
         Private tags start with an underscore.<br>
         (i.e. "science", "my/tag", or "_my/private/tag")
       </span>
     </ng-template>
     <ng-template #qtag>
       <span *ngIf="field.type === 'qtag' && field.formControl?.errors?.['pattern'] else query" i18n>
-        Tags must be lower case letters, numbers, periods and forward slashes.
-        Must not start with a forward slash or period.
-        Must not or contain two forward slashes or periods in a row.
-        Use the local wildcard (*) to match all tags with a local origin.
-        Tags may be qualified with an origin, or the default origin (@).
-        Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.
-        Use an origin without a tag to match all tags at that origin.
-        The wildcard origin (@*) by itself will match everything.
-        Protected tags start with a plus sign.
+        Tags must be lower case letters, numbers, periods and forward slashes.<br>
+        Must not start with a forward slash or period.<br>
+        Must not or contain two forward slashes or periods in a row.<br>
+        Tags may be qualified with an origin.<br>
+        Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.<br>
+        Protected tags start with a plus sign.<br>
         Private tags start with an underscore.<br>
-        (i.e. "*", "science", "science@origin" "my/tag@", or "_my/private/tag")
+        (i.e. "science", "science@origin" "my/tag", or "_my/private/tag")
       </span>
     </ng-template>
     <ng-template #query>
       <span *ngIf="field.type === 'query' && field.formControl?.errors?.['pattern'] else selector" i18n>
-        Queries support AND (:), OR (|), NOT (!) and grouping qualified tags (parentheses).
-        Tags must be lower case letters, numbers, periods and forward slashes.
-        Must not start with a forward slash or period.
-        Must not or contain two forward slashes or periods in a row.
-        Use the local wildcard (*) to match all tags with a local origin.
-        Tags may be qualified with an origin, or a wildcard origin (@*).
-        Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.
-        Use an origin without a tag to match all tags at that origin.
-        The wildcard origin (@*) by itself will match everything.
-        Protected tags start with a plus sign.
+        Queries support AND (:), OR (|), NOT (!) and grouping qualified tags (parentheses).<br>
+        Tags must be lower case letters, numbers, periods and forward slashes.<br>
+        Must not start with a forward slash or period.<br>
+        Must not or contain two forward slashes or periods in a row.<br>
+        Use the local wildcard (*) to match all tags with a local origin.<br>
+        Tags may be qualified with an origin, or a wildcard origin (@*).<br>
+        Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.<br>
+        Use an origin without a tag to match all tags at that origin.<br>
+        The wildcard origin (@*) by itself will match everything.<br>
+        Protected tags start with a plus sign.<br>
         Private tags start with an underscore.<br>
         (i.e. "science:news", "science@origin science@other" "your/tag my/tag", "!cool", or "news:_my/private/tag")
       </span>
     </ng-template>
     <ng-template #selector>
       <span *ngIf="field.type === 'selector' && field.formControl?.errors?.['pattern'] else user" i18n>
-        Tags must be lower case letters, numbers, periods and forward slashes.
-        Must not start with a forward slash or period.
-        Must not or contain two forward slashes or periods in a row.
-        Use the local wildcard (*) to match all tags with a local origin.
-        Tags may be qualified with an origin, or a wildcard origin (@*).
-        Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.
-        Use an origin without a tag to match all tags at that origin.
-        The wildcard origin (@*) by itself will match everything.
-        Protected tags start with a plus sign.
+        Tags must be lower case letters, numbers, periods and forward slashes.<br>
+        Must not start with a forward slash or period.<br>
+        Must not or contain two forward slashes or periods in a row.<br>
+        Use the local wildcard (*) to match all tags with a local origin.<br>
+        Tags may be qualified with an origin, or a wildcard origin (@*).<br>
+        Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.<br>
+        Use an origin without a tag to match all tags at that origin.<br>
+        The wildcard origin (@*) by itself will match everything.<br>
+        Protected tags start with a plus sign.<br>
         Private tags start with an underscore.<br>
-        (i.e. "*", "!science", "science@origin" "my/tag@", or "_my/private/tag")
+        (i.e. "*", "science", "science@origin" "my/tag@", or "_my/private/tag")
       </span>
     </ng-template>
     <ng-template #user>
-      <span *ngIf="field.type === 'user' && field.formControl?.errors?.['pattern'] else defaultMessage" i18n>
-      User tags must start with the "+user/" or "_user/" prefix.
+      <span *ngIf="field.type === 'user' && field.formControl?.errors?.['pattern'] else quser" i18n>
+      User tags must start with the "+user/" or "_user/" prefix.<br>
       Tags must be lower case letters and forward slashes. Must not start with a slash or contain two forward slashes in a row. Private
       tags start with an underscore.<br>
+      (i.e. "+user/alice", "_user/bob", or "+user/department/charlie")
+      </span>
+    </ng-template>
+    <ng-template #quser>
+      <span *ngIf="field.type === 'quser' && field.formControl?.errors?.['pattern'] else defaultMessage" i18n>
+      User tags must start with the "+user/" or "_user/" prefix.<br>
+      Tags must be lower case letters and forward slashes. Must not start with a slash or contain two forward slashes in a row. Private
+      tags start with an underscore.<br>
+      Tags may be qualified with an origin.<br>
+      Origins must start with an at sign (@) and contain only lowercase letters, numbers, and periods.<br>
       (i.e. "+user/alice", "_user/bob", or "+user/department/charlie")
       </span>
     </ng-template>
