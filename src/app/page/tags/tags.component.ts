e73757ee2c244248b6f30ca085071dca925a7a66
Chris Malloy <cjmalloy@gmail.com>
2023-09-24 18:37:56 -0300

    Set default tag sort

--- src/app/page/tags/tags.component.ts
+++ src/app/page/tags/tags.component.ts
@@ -28,7 +28,7 @@ export class TagsPage implements OnInit, OnDestroy {
     public query: ExtStore,
   ) {
     theme.setTitle($localize`Tags`);
-    store.view.clear('levels', 'levels');
+    store.view.clear('modified,DESC', 'name,ASC');
     query.clear();
   }
 
