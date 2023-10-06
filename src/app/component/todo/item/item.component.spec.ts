000337df97574721dc42b4d43502ef0a6ec0f93f
Chris Malloy <cjmalloy@gmail.com>
2023-10-06 20:14:02 -0300

    Fixed tests

--- src/app/component/todo/item/item.component.spec.ts
+++ src/app/component/todo/item/item.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TodoItemComponent } from './item.component';
 
@@ -8,7 +10,11 @@ describe('ItemComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TodoItemComponent]
+      declarations: [TodoItemComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ]
     });
     fixture = TestBed.createComponent(TodoItemComponent);
     component = fixture.componentInstance;
--- src/app/component/todo/todo.component.spec.ts
+++ src/app/component/todo/todo.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TodoComponent } from './todo.component';
 
@@ -8,7 +10,11 @@ describe('TodoComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TodoComponent]
+      declarations: [TodoComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ]
     });
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
