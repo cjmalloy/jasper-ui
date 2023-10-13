fa6de452f42576a01664f2f5dab32292b6eae332
Chris Malloy <cjmalloy@gmail.com>
2023-10-13 16:34:39 -0300

    Auth uses origin in tag instead of origin role maps

--- src/app/service/debug.service.ts
+++ src/app/service/debug.service.ts
@@ -32,11 +32,6 @@ export class DebugService {
     if (tag.startsWith('_') && !roles.includes('ROLE_PRIVATE')) {
       roles.push('ROLE_PRIVATE');
     }
-    let origin = '';
-    if (tag.includes('@')) {
-      origin = tag.substring(tag.indexOf('@'));
-      tag = tag.substring(0, tag.indexOf('@'));
-    }
     const header = {
       alg: 'HS256',
       typ: 'JWT'
@@ -44,13 +39,8 @@ export class DebugService {
     const payload = {
       aud: '',
       sub: '+user'.length === tag.length ? tag : tag.substring('+user/'.length),
-      auth: {} as any,
+      auth: roles.join(','),
     };
-    if (origin) {
-      payload.auth[origin] = roles.join(',');
-    } else {
-      payload.auth = roles.join(',');
-    }
     const body = btoa(JSON.stringify(header)) + '.' + btoa(JSON.stringify(payload));
 
     const secret = atob('MjY0ZWY2ZTZhYmJhMTkyMmE5MTAxMTg3Zjc2ZDlmZWUwYjk0MDgzODA0MDJiOTgyNTk4MmNjYmQ4Yjg3MmVhYjk0MmE0OGFmNzE2YTQ5ZjliMTEyN2NlMWQ4MjA5OTczYjU2NzAxYTc4YThkMzYxNzdmOTk5MTIxODZhMTkwMDM=');
