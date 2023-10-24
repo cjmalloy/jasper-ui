5ea65132bf0c093e142b141f13f7cd1fecaab1e4
Chris Malloy <cjmalloy@gmail.com>
2023-10-24 07:31:56 -0300

    Fixes tests

--- src/app/component/backgammon/backgammon.component.spec.ts
+++ src/app/component/backgammon/backgammon.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
 
 import { BackgammonComponent } from './backgammon.component';
 
@@ -8,7 +11,14 @@ describe('BackgammonComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [BackgammonComponent]
+      declarations: [BackgammonComponent], imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: OAuthService, useValue: {} },
+        { provide: OAuthStorage, useValue: {} },
+      ],
     });
     fixture = TestBed.createComponent(BackgammonComponent);
     component = fixture.componentInstance;
