5420ccb065d4f394e1b844caf3f65c65947acde0
Chris Malloy <cjmalloy@gmail.com>
2023-11-24 12:53:47 +0900

    Fixed tests

--- src/app/directive/resize.directive.spec.ts
+++ src/app/directive/resize.directive.spec.ts
@@ -2,7 +2,7 @@ import { ResizeDirective } from './resize.directive';
 
 describe('ResizeDirective', () => {
   it('should create an instance', () => {
-    const directive = new ResizeDirective({} as any);
+    const directive = new ResizeDirective({} as any, {} as any);
     expect(directive).toBeTruthy();
   });
 });
