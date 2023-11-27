e108136dd0c9f607491581bc430329ed3c1849fc
Chris Malloy <cjmalloy@gmail.com>
2023-11-27 18:04:24 +0900

    Support CSRF protection

---
+++ src/app/http/csrf.interceptor.spec.ts
@@ -0,0 +1,20 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { CsrfInterceptor } from './csrf.interceptor';
+
+describe('CsrfInterceptor', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      CsrfInterceptor
+    ],
+    imports: [
+      HttpClientTestingModule,
+    ]
+  }));
+
+  it('should be created', () => {
+    const interceptor: CsrfInterceptor = TestBed.inject(CsrfInterceptor);
+    expect(interceptor).toBeTruthy();
+  });
+});
---
+++ src/app/http/csrf.interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable, isDevMode } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfigService } from '../service/config.service';
+
+@Injectable()
+export class CsrfInterceptor implements HttpInterceptor {
+
+  withCredentials = isDevMode() || this.config.electron;
+
+  constructor(
+    private config: ConfigService,
+  ) {}
+
+  private getCsrfToken(): string {
+    return document.cookie.split('; ').find(row => row.startsWith('XSRF-TOKEN='))?.split('=')?.[1] || '';
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.method === 'GET' || request.method === 'HEAD' || request.method === 'OPTIONS') {
+      return next.handle(request);
+    }
+
+    const modifiedReq = request.clone({
+      headers: request.headers.set('X-CSRF-TOKEN', this.getCsrfToken()),
+      withCredentials: this.withCredentials,
+    });
+    return next.handle(modifiedReq);
+  }
+}
