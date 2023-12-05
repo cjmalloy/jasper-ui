d16248b9200a33f60b91caf0ab4bffd9fd7c65b0
Chris Malloy <cjmalloy@gmail.com>
2023-12-05 16:33:06 +0900

    Don't try to patch remote refs when deleting

--- src/app/component/ref/ref.component.ts
+++ src/app/component/ref/ref.component.ts
@@ -924,9 +924,11 @@ export class RefComponent implements OnChanges, AfterViewInit, OnDestroy {
 
   delete$ = () => {
     this.serverError = [];
-    return (hasTag('locked', this.ref) ? this.ts.patch(['plugin/delete', 'internal'], this.ref.url, this.ref.origin).pipe(map(() => {}))
-      : this.admin.getPlugin('plugin/delete') ? this.refs.update(deleteNotice(this.ref)).pipe(map(() => {}))
-      : this.refs.delete(this.ref.url, this.ref.origin)
+    return (this.local && hasTag('locked', this.ref)
+        ? this.ts.patch(['plugin/delete', 'internal'], this.ref.url, this.ref.origin).pipe(map(() => {}))
+        : this.local && this.admin.getPlugin('plugin/delete')
+        ? this.refs.update(deleteNotice(this.ref)).pipe(map(() => {}))
+        : this.refs.delete(this.ref.url, this.ref.origin)
     ).pipe(
       tap(() => this.deleted = true),
       catchError((err: HttpErrorResponse) => {
