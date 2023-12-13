14693fbc6af3c0f3240f9efba6a09f61d1848e33
Chris Malloy <cjmalloy@gmail.com>
2023-12-13 19:37:24 +0900

    Add public tag by default

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -115,9 +115,9 @@ export class SidebarComponent implements OnInit, OnChanges, OnDestroy {
         this.localTag = localTag(this.tag);
         this.plugin = this.admin.getPlugin(this.tag);
         if (this.home) {
-          this.addTags = this.rootConfig?.addTags || [];
+          this.addTags = this.rootConfig?.addTags || ['public'];
         } else {
-          this.addTags = uniq([...this.rootConfig?.addTags || (this.plugin?.tag ? [this.plugin!.tag] : []), ...topAnds(this.tag).map(localTag)]);
+          this.addTags = uniq([...this.rootConfig?.addTags || (this.plugin?.tag ? [this.plugin!.tag, 'public'] : ['public']), ...topAnds(this.tag).map(localTag)]);
         }
         this.defaultThumbnail = this.rootConfig?.defaultThumbnail;
         this.mailPlugin = this.admin.getPlugin(getMailbox(this.tag, this.store.account.origin));
