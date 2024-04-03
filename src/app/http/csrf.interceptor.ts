cc4a02b9b55d3af564b3d13b90d9a95e4ffcc76f
Chris Malloy <cjmalloy@gmail.com>
2024-04-03 11:59:46 -0300

    Retry CSRF if first one failed

--- src/app/http/csrf.interceptor.ts
+++ src/app/http/csrf.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable, isDevMode } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, filter, Observable, retry, retryWhen, switchMap, throwError } from 'rxjs';
 import { ConfigService } from '../service/config.service';
 
 @Injectable()
@@ -25,6 +25,17 @@ export class CsrfInterceptor implements HttpInterceptor {
       headers: request.headers.set('X-XSRF-TOKEN', this.getCsrfToken()),
       withCredentials: this.withCredentials,
     });
-    return next.handle(modifiedReq);
+    return next.handle(modifiedReq).pipe(
+      catchError(err => {
+        if (!err.status || err.status === 403) {
+          const retryReq = request.clone({
+            headers: request.headers.set('X-XSRF-TOKEN', this.getCsrfToken()),
+            withCredentials: this.withCredentials,
+          });
+          return next.handle(retryReq);
+        }
+        return throwError(() => err);
+      }),
+    );
   }
 }
