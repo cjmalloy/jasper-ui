c4e8b94cb4a7abbcccedc967fc29f2c3ee6863e3
Chris Malloy <cjmalloy@gmail.com>
2024-07-08 17:23:31 -0300

    Cypress: Ignore resize observer errors

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -48,5 +48,8 @@ Cypress.on('uncaught:exception', (err) => {
   if (err.message.includes('ResizeObserver loop limit exceeded')) {
     return false;
   }
+  if (err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
+    return false;
+  }
   return true;
 });
