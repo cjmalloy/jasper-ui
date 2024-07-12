e249c8195cdcc21b854df2aad5aabafd595c5555
Chris Malloy <cjmalloy@gmail.com>
2024-07-11 22:42:51 -0300

    Prevent local OOM

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   viewportWidth: 1025, // Prevent mobile layout
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    // baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://localhost:8080',
     experimentalRunAllSpecs: true,
     defaultCommandTimeout: 10_000,
+    numTestsKeptInMemory: 0,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
