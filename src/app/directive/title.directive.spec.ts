20c205f1cbf9c0d14e97f3ed8b0efac6ecad6148
Chris Malloy <cjmalloy@gmail.com>
2024-08-05 17:29:39 -0300

    Fixed tests

--- src/app/directive/title.directive.spec.ts
+++ src/app/directive/title.directive.spec.ts
@@ -2,7 +2,7 @@ import { TitleDirective } from './title.directive';
 
 describe('TitleDirective', () => {
   it('should create an instance', () => {
-    const directive = new TitleDirective({} as any);
+    const directive = new TitleDirective({} as any, {} as any);
     expect(directive).toBeTruthy();
   });
 });
