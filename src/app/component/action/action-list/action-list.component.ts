5b16563eae3c941843c9cf9e95fe444f9b7fb426
Chris Malloy <cjmalloy@gmail.com>
2024-10-10 19:00:05 -0300

    Hide actions in overflow

--- src/app/component/action/action-list/action-list.component.ts
+++ src/app/component/action/action-list/action-list.component.ts
@@ -1,27 +1,30 @@
 import { Overlay, OverlayRef } from '@angular/cdk/overlay';
 import { TemplatePortal } from '@angular/cdk/portal';
 import {
-  ChangeDetectionStrategy,
+  AfterViewInit,
   Component,
+  ElementRef,
+  HostListener,
   Input,
   NgZone,
   TemplateRef,
   ViewChild,
   ViewContainerRef
 } from '@angular/core';
-import { defer } from 'lodash-es';
+import { debounce, defer, delay } from 'lodash-es';
 import { Subscription } from 'rxjs';
 import { Ref, writeRef } from '../../../model/ref';
 import { Action } from '../../../model/tag';
 import { ActionService } from '../../../service/action.service';
 import { downloadRef } from '../../../util/download';
+import { memo } from '../../../util/memo';
 
 @Component({
   selector: 'app-action-list',
   templateUrl: './action-list.component.html',
   styleUrl: './action-list.component.scss'
 })
-export class ActionListComponent {
+export class ActionListComponent implements AfterViewInit {
 
   @Input()
   ref!: Ref;
@@ -39,16 +42,24 @@ export class ActionListComponent {
   @ViewChild('actionsMenu')
   actionsMenu!: TemplateRef<any>;
 
+  hiddenActions = 0;
   overlayRef?: OverlayRef;
 
   private overlayEvents?: Subscription;
+  private resizeObserver = window.ResizeObserver && new ResizeObserver(() => this.onResize()) || undefined;
 
   constructor(
     private acts: ActionService,
     private overlay: Overlay,
+    private el: ElementRef<HTMLElement>,
     private viewContainerRef: ViewContainerRef,
     private zone: NgZone,
   ) { }
+
+  ngAfterViewInit() {
+    this.resizeObserver?.observe(this.el.nativeElement!.parentElement!);
+    this.onResize();
+  }
 
   apply$ = (actions: Action[]) => () => {
     this.closeAdvanced();
@@ -62,6 +73,52 @@ export class ActionListComponent {
   downloadMedia() {
     if (!this.mediaAttachment) return;
     window.open(this.mediaAttachment, "_blank");
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (!this.actions) return;
+    this.measureVisible();
+  }
+
+  measureVisible = debounce(() => {
+    if (!this.actions) return;
+    defer(() => this.hiddenActions = this.actions - this.visible);
+    delay(() => this.hiddenActions = this.actions - this.visible, 500);
+  }, 400);
+
+  @memo
+  get actions() {
+    return Object.keys(this.groupedActions as any).length;
+  }
+
+  @memo
+  get actionWidths() {
+    const el = this.el.nativeElement;
+    const result: number[] = [];
+    for (let i = 0; i < el!.children.length; i++) {
+      const e = el.parentElement!.children[i] as HTMLElement;
+      const s = getComputedStyle(e);
+      result.push(e.offsetWidth + parseInt(s.marginLeft) + parseInt(s.marginRight));
+    }
+    return result;
+  }
+
+  get visible() {
+    const el = this.el.nativeElement;
+    const parentWidth = el.parentElement!.offsetWidth;
+    let result = 0;
+    let childWidth = 0;
+    for (let i = 0; i < el.parentElement!.children.length - 1; i++) {
+      const e = el.parentElement!.children[i] as HTMLElement;
+      const s = getComputedStyle(e);
+      childWidth += e.offsetWidth + parseInt(s.marginLeft) + parseInt(s.marginRight);
+    }
+    for (const w of this.actionWidths) {
+      childWidth += w;
+      if (childWidth < parentWidth) result++;
+    }
+    return result;
   }
 
   showAdvanced(event: MouseEvent) {
