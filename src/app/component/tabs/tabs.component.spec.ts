a69d8fcec2b1c56f5a8d42b1c269d210d290bdf7
Chris Malloy <cjmalloy@gmail.com>
2024-10-17 14:32:30 -0300

    Fixed tests

--- src/app/component/tabs/tabs.component.spec.ts
+++ src/app/component/tabs/tabs.component.spec.ts
@@ -1,4 +1,7 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PluginsFormComponent } from '../../form/plugins/plugins.component';
 
 import { TabsComponent } from './tabs.component';
 
@@ -8,9 +11,12 @@ describe('TabsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TabsComponent]
-    })
-    .compileComponents();
+      declarations: [TabsComponent],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
