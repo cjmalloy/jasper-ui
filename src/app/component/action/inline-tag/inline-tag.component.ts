97a687899a3d96474f2cf6724abbbf3fcfab056e
Chris Malloy <cjmalloy@gmail.com>
2024-12-30 17:28:19 -0400

    Added autocomplete to inline tag

--- src/app/component/action/inline-tag/inline-tag.component.ts
+++ src/app/component/action/inline-tag/inline-tag.component.ts
@@ -1,5 +1,11 @@
 import { Component, HostBinding, Input } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { debounce } from 'lodash-es';
+import { catchError, Observable, of, Subscription } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+import { Config } from '../../../model/tag';
+import { AdminService } from '../../../service/admin.service';
+import { ExtService } from '../../../service/api/ext.service';
+import { Store } from '../../../store/store';
 import { TAGS_REGEX } from '../../../util/format';
 import { ActionComponent } from '../action.component';
 
@@ -18,6 +24,19 @@ export class InlineTagComponent extends ActionComponent {
 
   editing = false;
   acting = false;
+  id = uuid();
+  autocomplete: { value: string; label: string }[] = [];
+
+  private searching?: Subscription;
+
+  constructor(
+    private store: Store,
+    private admin: AdminService,
+    private exts: ExtService,
+  ) {
+    super();
+  }
+
 
   override reset() {
     this.editing = false;
@@ -46,4 +65,30 @@ export class InlineTagComponent extends ActionComponent {
     ).subscribe(() => this.acting = false);
   }
 
+  search = debounce((input: HTMLInputElement) => {
+    const value = input.value = input.value.toLowerCase();
+    const toEntry = (p: Config) => ({ value: p.tag, label: p.name || p.tag });
+    const getPlugins = (text: string) => this.admin.searchPlugins(text).slice(0, 1).map(toEntry);
+    const getTemplates = (text: string) => this.admin.searchTemplates(text).slice(0, 1).map(toEntry);
+    this.searching?.unsubscribe();
+    this.searching = this.exts.page({
+      query: this.store.account.origin || '@',
+      search: value,
+      size: 1,
+    }).subscribe(page => {
+      this.autocomplete = page.content.map(x => ({ value: x.tag, label: x.name || x.tag }));
+      if (this.autocomplete.length < 1) this.autocomplete.push(...getPlugins(value));
+      if (this.autocomplete.length < 1) this.autocomplete.push(...getTemplates(value));
+    });
+  }, 400);
+
+  keydown(event: KeyboardEvent, input: HTMLInputElement) {
+    if (event.key === 'Enter') {
+      this.save(input)
+    }
+    if (event.key === 'Tab' && this.autocomplete.length) {
+      input.value = this.autocomplete[0].value;
+      event.preventDefault();
+    }
+  }
 }
