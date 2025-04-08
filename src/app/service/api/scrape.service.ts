d308a6dccc832c77b7dfe379799ff2cf7ab0c527
Chris Malloy <cjmalloy@gmail.com>
2025-04-07 21:08:39 -0300

    Bugfix for saving catch-all off default origin

--- src/app/service/api/scrape.service.ts
+++ src/app/service/api/scrape.service.ts
@@ -58,9 +58,9 @@ export class ScrapeService {
   }
 
   defaults(): Observable<any> {
-    return this.refs.update(catchAll, true).pipe(
+    return this.refs.update({ ...catchAll, origin: this.store.account.origin }, true).pipe(
       catchError(err => {
-        if (err.status === 404) return this.refs.create(catchAll, true);
+        if (err.status === 404) return this.refs.create({ ...catchAll, origin: this.store.account.origin }, true);
         return throwError(() => err);
       })
     );
