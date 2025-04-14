b3f7887657d97805ca05867abdc397b8e8a5a531
Chris Malloy <cjmalloy@gmail.com>
2025-04-13 21:11:18 -0300

    Bugfix for removing input on focus bubble

--- src/app/formly/list.type.ts
+++ src/app/formly/list.type.ts
@@ -150,9 +150,15 @@ export class ListTypeComponent extends FieldArrayType {
     }
   }
 
+  /**
+   * Remove blank inputs on blur.
+   */
   maybeRemove(event: FocusEvent, i: number) {
     if (this.groupArray) return;
-    if (!(event.target as any).value) this.remove(i);
+    const input = event.target as HTMLInputElement;
+    if (input.tagName !== 'INPUT') return;
+    if (input.classList.contains('preview')) return;
+    if (!input.value) this.remove(i);
   }
 
   focus(index?: number, select = false) {
