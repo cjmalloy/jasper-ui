fc09c6eced32266b356e354ec7b75a6479d4af2b
Chris Malloy <cjmalloy@gmail.com>
2025-06-30 17:11:20 -0300

    platformBrowserDynamic deprecated

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /// <reference types="@angular/localize" />
 
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -12,5 +12,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser().bootstrapModule(AppModule)
   .catch(err => console.error(err));
