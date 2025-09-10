296eef3c9bc98d61dce370ec9a40f5d587b4c9fd
Chris Malloy <cjmalloy@gmail.com>
2025-09-09 21:46:37 -0300

    Default to en

--- docker/00-select-locale.sh
+++ docker/00-select-locale.sh
@@ -3,5 +3,9 @@
 LOCALE=${JASPER_LOCALE:-en}
 echo "Deploying locale: $LOCALE"
 
+if [ ! -d "/var/lib/jasper/$LOCALE" ]; then
+  echo "Locale $LOCALE does not exist, defaulting to en"
+  LOCALE="en"
+
 rm -rf /usr/share/nginx/html
 ln -s /var/lib/jasper/$LOCALE /usr/share/nginx/html
