79f747988df6be2c7ce811240bb24ed1605fb551
Chris Malloy <cjmalloy@gmail.com>
2025-09-19 18:50:12 -0300

    Fixed tests

--- src/app/component/user-tag-selector/user-tag-selector.component.spec.ts
+++ src/app/component/user-tag-selector/user-tag-selector.component.spec.ts
@@ -1,4 +1,8 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { AuthInterceptor } from '../../http/auth.interceptor';
 
 import { UserTagSelectorComponent } from './user-tag-selector.component';
 
@@ -8,7 +12,13 @@ describe('UserTagSelectorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserTagSelectorComponent ]
+      declarations: [ UserTagSelectorComponent ],
+      imports: [RouterModule.forRoot([])],
+      providers: [
+        AuthInterceptor,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting()
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserTagSelectorComponent);
