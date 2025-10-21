ed24f89420ef1891ac8799807c6202ddb300e0a6
Chris Malloy <cjmalloy@gmail.com>
2025-10-21 00:23:23 -0300

    Fix return value handling in RSS scrape

--- src/app/page/submit/web/web.component.ts
+++ src/app/page/submit/web/web.component.ts
@@ -137,7 +137,7 @@ export class SubmitWebPage implements AfterViewInit, OnDestroy, HasChanges {
           this.addFeedTags(...interestingTags(this.store.submit.tags));
           this.scrape.rss(url).pipe(
             switchMap(value => {
-              if (!value) return of();
+              if (!value) return of(value);
               return this.refs.page({ url: value, size: 1, query: this.store.account.origin, obsolete: null }).pipe(
                 map(page => page.content.length > 0 ? undefined : value),
                 catchError(() => of(value))
