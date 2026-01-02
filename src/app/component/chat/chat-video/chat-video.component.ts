45c066bc652077166c2a0352f5f283792a9e3d6b
Chris Malloy <cjmalloy@gmail.com>
2026-01-02 01:11:29 -0400

    Square

--- src/app/component/chat/chat-video/chat-video.component.ts
+++ src/app/component/chat/chat-video/chat-video.component.ts
@@ -50,7 +50,7 @@ export class ChatVideoComponent implements AfterViewInit {
   call() {
     runInAction(() => this.store.video.enabled = true);
     this.ts.respond(['public', 'plugin/user/lobby'], this.url).subscribe();
-    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    navigator.mediaDevices.getUserMedia({ audio: true, video: { width: { ideal: 640 }, height: { ideal: 640 } } })
       .then(stream => {
         if (!this.store.video.enabled) {
           stream.getTracks().forEach(t => t.stop());
