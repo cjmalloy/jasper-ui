039851fdd485d2006134c5fd59cd7647c746c631
Chris Malloy <cjmalloy@gmail.com>
2026-02-01 16:59:28 -0400

    Fixed circular

--- src/app/form/ref/ref.component.spec.ts
+++ src/app/form/ref/ref.component.spec.ts
@@ -1,6 +1,8 @@
 /// <reference types="vitest/globals" />
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { forwardRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import { provideRouter } from '@angular/router';
@@ -17,9 +19,9 @@ describe('RefFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        forwardRef(() => RefFormComponent),
         ReactiveFormsModule,
         JasperFormlyModule,
-        RefFormComponent
       ],
       providers: [
         PluginsFormComponent,
