63c442c9cf0379d1e7f4e3e214aac5a57c1829ae
Chris Malloy <cjmalloy@gmail.com>
2026-02-02 01:17:26 -0400

    Fixed query updates

--- src/app/store/query.ts
+++ src/app/store/query.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { isEqual, omit } from 'lodash-es';
 import { catchError, Subscription, throwError } from 'rxjs';
 import { Page } from '../model/page';
@@ -11,11 +11,12 @@ import { RefService } from '../service/api/ref.service';
 })
 export class QueryStore {
 
-  private _args = signal<RefPageArgs | undefined>(undefined);
+  private _args = signal<RefPageArgs | undefined>(undefined, { equal: isEqual });
   private _sourcesOf = signal<Ref | undefined>(undefined);
   private _responseOf = signal<Ref | undefined>(undefined);
   private _page = signal<Page<Ref> | undefined>(undefined);
   private _error = signal<HttpErrorResponse | undefined>(undefined);
+  private _refresh = signal(0);
 
   private running?: Subscription;
   private runningSources?: Subscription;
@@ -23,7 +24,30 @@ export class QueryStore {
 
   constructor(
     private refs: RefService,
-  ) {}
+  ) {
+    effect(() => {
+      const args = this._args();
+      this._refresh(); // Track refresh signal
+      if (!args) return;
+      this.running?.unsubscribe();
+      this.running = this.refs.page(args).pipe(
+        catchError((err: HttpErrorResponse) => {
+          this._error.set(err);
+          return throwError(() => err);
+        }),
+      ).subscribe(p => this._page.set(p));
+      this.runningSources?.unsubscribe();
+      if (args.sources) {
+        this.runningSources = this.refs.getCurrent(args.sources)
+          .subscribe(ref => this._sourcesOf.set(ref));
+      }
+      this.runningResponses?.unsubscribe();
+      if (args.responses) {
+        this.runningResponses = this.refs.getCurrent(args.responses)
+          .subscribe(ref => this._responseOf.set(ref));
+      }
+    });
+  }
 
   get args() { return this._args(); }
   set args(value: RefPageArgs | undefined) { this._args.set(value); }
@@ -58,29 +82,9 @@ export class QueryStore {
   setArgs(args: RefPageArgs) {
     if (!isEqual(omit(this._args(), 'search'), omit(args, 'search'))) this.clear();
     this._args.set(args);
-    this.refresh();
   }
 
   refresh() {
-    const args = this._args();
-    if (args) {
-      this.running?.unsubscribe();
-      this.running = this.refs.page(args).pipe(
-        catchError((err: HttpErrorResponse) => {
-          this._error.set(err);
-          return throwError(() => err);
-        }),
-      ).subscribe(p => this._page.set(p));
-      this.runningSources?.unsubscribe();
-      if (args.sources) {
-        this.runningSources = this.refs.getCurrent(args.sources)
-          .subscribe(ref => this._sourcesOf.set(ref));
-      }
-      this.runningResponses?.unsubscribe();
-      if (args.responses) {
-        this.runningResponses = this.refs.getCurrent(args.responses)
-          .subscribe(ref => this._responseOf.set(ref));
-      }
-    }
+    this._refresh.update(n => n + 1);
   }
 }
