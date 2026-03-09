e6c42ab4481be6c9fbf3ef8df9cd147441325cd8
Chris Malloy <cjmalloy@gmail.com>
2026-03-08 23:50:41 -0300

    Cleaned up

--- src/app/service/admin.service.ts
+++ src/app/service/admin.service.ts
@@ -26,7 +26,7 @@ import { htmlMod, latexMod } from '../mods/editor';
 import { experimentsMod } from '../mods/experiments';
 import { backgammonMod } from '../mods/games/backgammon';
 import { chessMod } from '../mods/games/chess';
-import { gridMod } from '../mods/grid';
+import { gridMod } from '../mods/org/grid';
 import { homeMod } from '../mods/home';
 import { lensMod } from '../mods/lens';
 import { mailboxMod } from '../mods/mailbox';
