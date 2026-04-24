52acbd43c0f2dd9f0c43296f97fb91a4b0d9ca99
Chris Malloy <cjmalloy@gmail.com>
2026-04-24 20:38:10 -0300

    Fix ngsw urls pattern, which is different than the ngsw-config pattern

--- docker/80-nsgw.sh
+++ docker/80-nsgw.sh
@@ -4,7 +4,7 @@ nsgw="/usr/share/nginx/html/ngsw.json"
 
 if [ -f "$nsgw" ]; then
     sed -i "s;\"/;\"$BASE_HREF;" "$nsgw"
-    jq ".dataGroups[0].patterns[0] = \"$JASPER_API\"" "$nsgw" | sponge "$nsgw"
+    jq ".dataGroups[0].patterns[0] = \"${JASPER_API}/api/.*\"" "$nsgw" | sponge "$nsgw"
 else
     echo "Service worker not found."
 fi
