92e95c15d0ce18d38f54e7466946eca9f28701c8
Chris Malloy <cjmalloy@gmail.com>
2026-05-05 17:46:57 -0300

    Filename hint breaks cache for thumbnails
    The filename hint is only useful for downloaded media.

--- src/app/pipe/thumbnail.pipe.ts
+++ src/app/pipe/thumbnail.pipe.ts
@@ -4,7 +4,6 @@ import { Ref } from '../model/ref';
 import { AdminService } from '../service/admin.service';
 import { ProxyService } from '../service/api/proxy.service';
 import { OembedStore } from '../store/oembed';
-import { getExtension } from '../util/http';
 import { hasTag } from '../util/tag';
 
 @Pipe({
@@ -23,13 +22,13 @@ export class ThumbnailPipe implements PipeTransform {
     for (const ref of refs) {
       if (!ref) continue;
       for (const plugin of ['plugin/thumbnail', 'plugin/image', 'plugin/video']) {
-        if (refUrl(ref, plugin)) return of(this.fetchUrl(refUrl(ref, plugin), ref.origin, ref.title || plugin.substring('plugin/'.length), plugin));
+        if (refUrl(ref, plugin)) return of(this.fetchUrl(refUrl(ref, plugin), ref.origin, plugin));
       }
       if (hasTag('plugin/embed', ref)) {
         return this.store.get(ref.plugins?.['plugin/embed']?.url || ref.url).pipe(
           map(oembed => {
             if (oembed?.thumbnail_url) {
-              return this.fetchUrl(oembed.thumbnail_url, ref.origin, ref.title || oembed.title || 'thumbnail', 'plugin/thumbnail');
+              return this.fetchUrl(oembed.thumbnail_url, ref.origin, 'plugin/thumbnail');
             }
             return '';
           }),
@@ -38,23 +37,22 @@ export class ThumbnailPipe implements PipeTransform {
       if (!this.validUrl(ref.url)) continue;
       const embedPlugins = this.admin.getEmbeds(ref);
       for (const plugin of ['plugin/image', 'plugin/video']) {
-        if (embedPlugins.includes(plugin)) return of(this.fetchUrl(ref.url, ref.origin, ref.title || plugin.substring('plugin/'.length), plugin));
+        if (embedPlugins.includes(plugin)) return of(this.fetchUrl(ref.url, ref.origin, plugin));
       }
     }
     if (force) {
       for (const ref of refs) {
         if (!this.validUrl(ref?.url)) continue;
-        return of(this.fetchUrl(ref!.url, ref!.origin, ref?.title || 'image', 'plugin/image'));
+        return of(this.fetchUrl(ref!.url, ref!.origin, 'plugin/image'));
       }
     }
     return of('');
   }
 
-  fetchUrl(url: string, origin: string | undefined, title: string, plugin: string) {
+  fetchUrl(url: string, origin: string | undefined, plugin: string) {
     if (!url) return '';
     if (url.startsWith('cache:') || this.admin.getPlugin(plugin)?.config?.proxy) {
-      const ext = getExtension(url) || '';
-      return this.proxy.getFetch(url, origin, title + (title.endsWith(ext) ? '' : ext), true);
+      return this.proxy.getFetch(url, origin, 'thumbnail', true);
     }
     return url;
   }
