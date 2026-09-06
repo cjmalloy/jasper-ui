55d07fb3f9e8b2e0e3209104d8be20f4180c2b39
Chris Malloy <cjmalloy@gmail.com>
2026-09-06 14:48:38 -0300

    Fix FileSaver usage to saveAs for downloads

--- src/app/util/download.ts
+++ src/app/util/download.ts
@@ -47,7 +47,7 @@ export async function downloadPage(type: Type, page: Page<any>, exts: Ext[], que
   zip.file(type + '.json', file(page.content!.map(write(type))));
   if (exts.length) zip.file('ext.json', file(exts.map(writeExt)));
   return zip.generateAsync({ type: 'blob' })
-    .then(content => FileSaver.(content, `${query.replace('/', '_')}` + (page.page.totalPages > 1 ? ` (page ${page.page.number + 1} of ${page.page.totalPages})` : '') + '.zip'));
+    .then(content => saveAs(content, `${query.replace('/', '_')}` + (page.page.totalPages > 1 ? ` (page ${page.page.number + 1} of ${page.page.totalPages})` : '') + '.zip'));
 }
 
 export async function downloadSet(ref: Ref[], ext: Ext[], title: string) {
