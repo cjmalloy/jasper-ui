8ac8d57b2de0ed934c8bb2ca3edc92d7b65a3a37
Chris Malloy <cjmalloy@gmail.com>
2026-09-13 19:44:58 -0300

    Fixed load more placeholder

--- src/app/component/embed-placeholder/embed-placeholder.component.ts
+++ src/app/component/embed-placeholder/embed-placeholder.component.ts
@@ -2,19 +2,15 @@ import { ChangeDetectionStrategy, Component, ViewChild, ViewContainerRef } from 
 
 @Component({
   selector: 'app-embed-placeholder',
-  styles: `:host { display: contents; }`,
-  host: { class: 'embed-placeholder' },
-  changeDetection: ChangeDetectionStrategy.Eager,
-  template: `
-    @if (!expanded) {
-      <button type="button" class="embed-expand" (click)="expand()" i18n>Embed one more level</button>
-    }
-    <ng-container #content></ng-container>
-  `,
+  templateUrl: 'embed-placeholder.component.html',
+  styleUrl: './embed-placeholder.component.scss',
+  changeDetection: ChangeDetectionStrategy.Eager
 })
 export class EmbedPlaceholderComponent {
+
   @ViewChild('content', { read: ViewContainerRef, static: true })
   content!: ViewContainerRef;
+
   create?: (vc: ViewContainerRef) => void;
   expanded = false;
 
