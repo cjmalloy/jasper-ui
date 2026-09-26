9e2962f2f8185937dbd78a9e23eb038d9e0e8a2d
Chris Malloy <cjmalloy@gmail.com>
2026-09-25 22:46:06 -0300

    Fix thread top with attachments (#1452)
    * Fix thread top with attachments
    
    * Fix condition checks for adding sources

--- src/app/page/submit/text/text.component.ts
+++ src/app/page/submit/text/text.component.ts
@@ -322,7 +322,14 @@ export class SubmitTextPage implements AfterViewInit, OnChanges, OnDestroy, HasC
     MemoCache.clear(this);
   }
 
+  get top() {
+    return this.sources.value[1] || this.sources.value[0] || this.ensureUrl();
+  }
+
   addSource(value = '') {
+    while (this.sources.value.length < 2) {
+      this.sources.push(this.fb.control(this.top, LinksFormComponent.validators));
+    }
     this.sources.push(this.fb.control(value, LinksFormComponent.validators));
     this.submitted = false;
   }
